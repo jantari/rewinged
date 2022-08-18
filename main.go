8c316f740e0608b5764e892b655e68da9acae418
jantari <jantari@github.com>
2022-08-18 17:32:15 +0200

    implement bare minimum /packageManifests GET response, winget download and install works

--- main.go
+++ main.go
@@ -25,6 +25,48 @@ type Package struct {
     PackageIdentifier string
 }
 
+type Manifest struct {
+    PackageIdentifier string
+    Versions []Versions
+}
+
+type Versions struct {
+    PackageVersion string
+    DefaultLocale Locale
+    Channel string
+    Locales []Locale
+    Installers []Installer
+}
+
+type Installer struct {
+    Architecture Architecture
+    InstallerType InstallerType
+    InstallerUrl string
+    InstallerSha256 string
+}
+
+type Locale struct {
+    PackageLocale string
+//    Moniker // Is this needed for DefaultLocale?
+    Publisher string
+//    PublisherUrl
+//    PublisherSupportUrl
+//    PrivacyUrl
+//    Author
+    PackageName string
+//    PackageUrl
+    License string
+//    LicenseUrl
+//    Copyright
+//    CopyrightUrl
+    ShortDescription string
+//    Description
+//    Tags
+//    ReleaseNotes
+//    ReleaseNotesUrl
+//    Agreements
+}
+
 type PackageMultipleResponse struct {
     Data []Package
     ContinuationToken string
@@ -36,6 +78,32 @@ type Information struct {
         ServerSupportedVersions []string
     }
 }
+
+type Architecture string
+
+const (
+    neutral Architecture = "neutral"
+    x86 = "x86"
+    x64 = "x64"
+    arm = "arm"
+    arm64 = "arm64"
+)
+
+type InstallerType string
+
+const (
+    msix InstallerType = "msix"
+    msi = "msi"
+    appx = "appx"
+    exe = "exe"
+    zip = "zip"
+    inno = "inno"
+    nullsoft = "nullsoft"
+    wix = "wix"
+    burn = "burn"
+    pwa = "pwa"
+    msstore = "msstore"
+)
 
 type MatchType string
 
@@ -61,6 +129,14 @@ const (
     ProductCode = "ProductCode"
     NormalizedPackageNameAndPublisher = "NormalizedPackageNameAndPublisher"
     Market = "Market"
+)
+
+type QueryParameter string
+
+const (
+    Version QueryParameter = "Version"
+    Channel = "Channel"
+//    Market = "Market" // Already declared in PackageMatchField enum
 )
 
 type SearchRequestMatch struct {
@@ -94,6 +170,12 @@ type ManifestSearchResponse struct {
     PackageName string
     Publisher string
     Versions []ManifestSearchVersion
+}
+
+type ManifestSingleResponse struct {
+    Data Manifest
+    RequiredQueryParameters []QueryParameter
+    UnsupportedQueryParameters []QueryParameter
 }
 
 type ManifestSearchResult struct {
@@ -142,12 +224,12 @@ func main() {
                 RequiredPackageMatchFields: []PackageMatchField{},
                 Data: []ManifestSearchResponse {
                     {
-                        PackageIdentifier: "kek",
-                        PackageName: "kek",
+                        PackageIdentifier: "bottom",
+                        PackageName: "bottom",
                         Publisher: "test",
                         Versions: []ManifestSearchVersion {
                             {
-                                PackageVersion: "1.0.0",
+                                PackageVersion: "0.6.8",
                             },
                         },
                     },
@@ -165,7 +247,36 @@ func main() {
     router.GET("/packageManifests/:package_identifier", func(c *gin.Context) {
         fmt.Println("/packageManifests: Someone tried to GET package '", c.Param("package_identifier"), "'")
         fmt.Println("with query params:", c.Request.URL.Query())
+        response := ManifestSingleResponse {
+            Data: Manifest {
+                PackageIdentifier: "bottom",
+                Versions: []Versions {
+                    {
+                        PackageVersion: "0.6.8",
+                        DefaultLocale: Locale {
+                            PackageLocale: "en-US",
+                            PackageName: "bottom",
+                            Publisher: "Clement Tsang",
+                            ShortDescription: "test package",
+                            License: "GPLv2",
+                        },
+                        Channel: "",
+                        Locales: []Locale{},
+                        Installers: []Installer {
+                            {
+                                Architecture: "x64",
+                                InstallerType: "msi",
+                                InstallerUrl: "https://github.com/ClementTsang/bottom/releases/download/0.6.8/bottom_x86_64_installer.msi",
+                                InstallerSha256: "43A860A1ECAC287CAF9745D774B0B2CE9C0A2A79D4048893E7649B0D8048EE58",
+                            },
+                        },
+                    },
+                },
+            },
+            RequiredQueryParameters: []QueryParameter{},
+            UnsupportedQueryParameters: []QueryParameter{},
+        }
+        c.JSON(200, response)
     })
-    //"kek?Version=1.0.0"
     router.RunTLS(":8080", "./cert.pem", "./server.key") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
