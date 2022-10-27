7175eb31e5894a94d411e5ee2fb674b8c39cacfd
jantari <jantari@github.com>
2022-10-28 00:30:45 +0200

    move all API-handler functions to controllers, searching functions to model

--- controllers/packages.go
+++ controllers/packages.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
     "fmt"
+    "log"
+
     "github.com/gin-gonic/gin"
 
     "rewinged/models"
@@ -15,3 +17,89 @@ func GetPackages(c *gin.Context) {
     fmt.Println(response)
     c.JSON(200, response)
 }
+
+func GetPackage(c *gin.Context) {
+  fmt.Println("/packageManifests: Someone tried to GET package '", c.Param("package_identifier"), "'")
+  fmt.Println("with query params:", c.Request.URL.Query())
+
+  response := models.ManifestSingleResponse {
+    RequiredQueryParameters: []models.QueryParameter{},
+    UnsupportedQueryParameters: []models.QueryParameter{},
+    Data: nil,
+  }
+
+  var pkg []models.Versions = models.Manifests.GetAllVersions(c.Param("package_identifier"))
+  if len(pkg) > 0 {
+    fmt.Println("the package was found!")
+
+    response.Data = &models.Manifest{
+      PackageIdentifier: c.Param("package_identifier"),
+      Versions: pkg,
+    }
+
+    c.JSON(200, response)
+  } else {
+    fmt.Println("the package was NOT found!")
+    c.JSON(404, models.WingetApiError{
+      ErrorCode: 404,
+      ErrorMessage: "The specified package was not found.",
+    })
+  }
+}
+
+func SearchForPackage(c *gin.Context) {
+  var post models.ManifestSearch
+  if err := c.BindJSON(&post); err == nil {
+    fmt.Printf("%+v\n", post)
+    response := &models.ManifestSearchResult{
+      RequiredPackageMatchFields: []models.PackageMatchField{},
+      Data: []models.ManifestSearchResponse {},
+    }
+
+    // results is a map where the PackageIdentifier is the key
+    // and the values are arrays of manifests with that PackageIdentifier.
+    // This means the values will be different versions of the package.
+    var results map[string][]models.Versions
+
+    if post.Query.KeyWord != "" {
+      fmt.Println("someone searched the repo for:", post.Query.KeyWord)
+      results = models.Manifests.GetByKeyword(post.Query.KeyWord)
+    } else if (post.Inclusions != nil && len(post.Inclusions) > 0) || (post.Filters != nil && len(post.Filters) > 0) {
+      fmt.Println("advanced search with inclusions[] and/or filters[]")
+      results = models.Manifests.GetByMatchFilter(post.Inclusions, post.Filters)
+    }
+
+    fmt.Println("... with", len(results), "results:")
+
+    if len(results) > 0 {
+      for packageId, packageVersions := range results {
+        fmt.Println("  package", packageId, "with", len(packageVersions), "versions.")
+        var versions []models.ManifestSearchVersion
+
+        for _, version := range packageVersions {
+          versions = append(versions, models.ManifestSearchVersion{
+            PackageVersion: version.PackageVersion,
+            Channel: "",
+            PackageFamilyNames: []string{},
+            ProductCodes: []string{ version.Installers[0].ProductCode },
+          })
+        }
+
+        response.Data = append(response.Data, models.ManifestSearchResponse{
+          PackageIdentifier: packageId,
+          PackageName: packageVersions[0].DefaultLocale.PackageName,
+          Publisher: packageVersions[0].DefaultLocale.Publisher,
+          Versions: versions,
+        })
+      }
+      fmt.Printf("%+v\n", response)
+      c.JSON(200, response)
+    } else {
+      // winget REST-API specification calls for a 204 return code if no results were found
+      c.JSON(204, response)
+    }
+  } else {
+    log.Println("error deserializing json post body %v\n", err)
+  }
+}
+
--- models/internal.go
+++ models/internal.go
@@ -1,6 +1,11 @@
 package models
 
-import "sync"
+import (
+    "fmt"
+    "sync"
+    "strings"
+    "reflect"
+)
 
 func getMapValues[M ~map[K]V, K comparable, V any](m M) []V {
     r := make([]V, 0, len(m))
@@ -8,6 +13,54 @@ func getMapValues[M ~map[K]V, K comparable, V any](m M) []V {
         r = append(r, v)
     }
     return r
+}
+
+func caseInsensitiveContains(s, substr string) bool {
+  s, substr = strings.ToUpper(s), strings.ToUpper(substr)
+  return strings.Contains(s, substr)
+}
+
+// Modified from: https://stackoverflow.com/a/38407429
+func findField(v interface{}, name string) reflect.Value {
+  // create queue of values to search. Start with the function arg.
+  queue := []reflect.Value{reflect.ValueOf(v)}
+  for len(queue) > 0 {
+    v := queue[0]
+    queue = queue[1:]
+    // dereference pointers
+    for v.Kind() == reflect.Ptr {
+        v = v.Elem()
+    }
+    // check all elements in slices
+    if v.Kind() == reflect.Slice {
+      if v.Len() > 0 {
+        for i := 0; i < v.Len(); i++ {
+          queue = append(queue, v.Index(i))
+        }
+      } else {
+        //fmt.Println("CONTINUE (EMPTY SLICE)")
+        continue
+      }
+    }
+    // ignore if this is not a struct
+    if v.Kind() != reflect.Struct {
+        //fmt.Println("CONTINUE (NOT STRUCT)")
+        continue
+    }
+    // iterate through fields looking for match on name
+    t := v.Type()
+    for i := 0; i < v.NumField(); i++ {
+        //fmt.Println("TESTING FIELD", t.Field(i).Name)
+        if t.Field(i).Name == name {
+            // found it!
+            //fmt.Println("FOUND THE FIELD", name, "!")
+            return v.Field(i)
+        }
+        // push field to queue
+        queue = append(queue, v.Field(i))
+    }
+  }
+  return reflect.Value{}
 }
 
 // Internal in-memory data store of all manifest data
@@ -69,6 +122,190 @@ func (ms *ManifestsStore) GetManifestCount() (value int) {
     count = len(Manifests.internal)
     ms.RUnlock()
     return count
+}
+
+func (ms *ManifestsStore) GetByKeyword (keyword string) map[string][]Versions {
+  var manifestResultsMap = make(map[string][]Versions)
+  ms.RLock()
+  for packageIdentifier, packageVersions := range ms.internal {
+    for _, version := range packageVersions {
+      if caseInsensitiveContains(version.DefaultLocale.PackageName, keyword) || caseInsensitiveContains(version.DefaultLocale.ShortDescription, keyword) {
+        manifestResultsMap[packageIdentifier] = append(manifestResultsMap[packageIdentifier], version)
+      }
+    }
+  }
+  ms.RUnlock()
+
+  return manifestResultsMap
+}
+
+func (ms *ManifestsStore) GetByMatchFilter (inclusions []SearchRequestPackageMatchFilter, filters []SearchRequestPackageMatchFilter) map[string][]Versions {
+  var manifestResultsMap = make(map[string][]Versions)
+  normalizeReplacer := strings.NewReplacer(" ", "", "-", "", "+", "")
+
+  ms.RLock()
+  for packageIdentifier, packageVersions := range ms.internal {
+    // Loop through every version of the package as well because MatchFields like
+    // ProductCode, PackageName etc. can change between versions.
+    NEXT_VERSION:
+    for _, packageVersion := range packageVersions {
+      // From what I can gather from https://github.com/microsoft/winget-cli-restsource/blob/01542050d79da0efbd11c0a5be543cb970b86eb9/src/WinGet.RestSource/Cosmos/CosmosDataStore.cs#L452
+      // the difference between inclusions and filters are that inclusions are evaluated with a logical OR (only one of them has to match) and filters are evaluated with a logical AND
+      // (all filter specified have to match) - so this is what I implemented here. But I am not 100% sure this is the correct/intended use for inclusions vs. filters.
+
+      // process filters (if any)
+      for _, filter := range filters {
+        var requestMatchValue string
+
+        switch filter.PackageMatchField {
+          case NormalizedPackageNameAndPublisher:
+            // winget only ever sends the package / software name, the publisher isn't included so to
+            // enable proper matching we also only compare against the normalized packagename.
+            requestMatchValue = normalizeReplacer.Replace(strings.ToLower(packageVersion.DefaultLocale.PackageName))
+          case PackageIdentifier:
+            // We don't need to recursively search for this field, it's easy to get to
+            requestMatchValue = packageIdentifier
+          case PackageName:
+            fallthrough
+          case Moniker:
+            fallthrough
+          case Command:
+            fallthrough
+          case Tag:
+            fallthrough
+          case PackageFamilyName:
+            fallthrough
+          case ProductCode:
+            fallthrough
+          case Market:
+            fallthrough
+          default:
+            // Just search the whole struct for a field with the right name
+            // Get the value of a nested struct field passing in the field name to search for as a string
+            // Source: https://stackoverflow.com/a/38407429
+            f := findField(packageVersion, string(filter.PackageMatchField))
+            requestMatchValue = string(f.String())
+        }
+
+        // Because all filters (if any) must match (logical AND)
+        // we just skip to the next packageversion if any did not match
+        switch filter.RequestMatch.MatchType {
+          // TODO: `winget list -s rewinged-local -q lapce` searches for the ProductCode with MatchType Exact
+          // Why does it use MatchType Exact?? Does the reference / official source normalize all ProductCodes on ingest??
+          case Exact:
+            if requestMatchValue != filter.RequestMatch.KeyWord {
+              continue NEXT_VERSION
+            }
+          case CaseInsensitive:
+            if !strings.EqualFold(requestMatchValue, filter.RequestMatch.KeyWord) {
+              continue NEXT_VERSION
+            }
+          case StartsWith:
+            // StartsWith is implemented as case-sensitive, because it is that way in the reference implementation as well:
+            // https://github.com/microsoft/winget-cli-restsource/blob/01542050d79da0efbd11c0a5be543cb970b86eb9/src/WinGet.RestSource/Cosmos/PredicateGenerator.cs#L92-L102
+            if !strings.HasPrefix(requestMatchValue, filter.RequestMatch.KeyWord) {
+              continue NEXT_VERSION
+            }
+          case Substring:
+            // Substring comparison is case-insensitive, because it is that way in the reference implementation as well:
+            // https://github.com/microsoft/winget-cli-restsource/blob/01542050d79da0efbd11c0a5be543cb970b86eb9/src/WinGet.RestSource/Cosmos/PredicateGenerator.cs#L92-L102
+            if !caseInsensitiveContains(requestMatchValue, filter.RequestMatch.KeyWord) {
+              continue NEXT_VERSION
+            }
+          default:
+            // Unimplemented: Wildcard, Fuzzy, FuzzySubstring
+        }
+      }
+
+      if len(inclusions) == 0 {
+        manifestResultsMap[packageIdentifier] = append(manifestResultsMap[packageIdentifier], packageVersion)
+        continue NEXT_VERSION
+      }
+
+      var anyInclusionMatched bool
+      anyInclusionMatched = true
+      // process inclusions (if any)
+      NEXT_INCLUSION:
+      for _, inclusion := range inclusions {
+        var requestMatchValue string
+        anyInclusionMatched = false
+
+        switch inclusion.PackageMatchField {
+          case NormalizedPackageNameAndPublisher:
+            // winget only ever sends the package / software name, the publisher isn't included so to
+            // enable proper matching we also only compare against the normalized packagename.
+            requestMatchValue = normalizeReplacer.Replace(strings.ToLower(packageVersion.DefaultLocale.PackageName))
+          case PackageIdentifier:
+            // We don't need to recursively search for this field, it's easy to get to
+            requestMatchValue = packageIdentifier
+          case PackageName:
+            fallthrough
+          case Moniker:
+            fallthrough
+          case Command:
+            fallthrough
+          case Tag:
+            fallthrough
+          case PackageFamilyName:
+            fallthrough
+          case ProductCode:
+            fallthrough
+          case Market:
+            fallthrough
+          default:
+            // Just search the whole struct for a field with the right name
+            // Get the value of a nested struct field passing in the field name to search for as a string
+            // Source: https://stackoverflow.com/a/38407429
+            f := findField(packageVersion, string(inclusion.PackageMatchField))
+            requestMatchValue = string(f.String())
+        }
+
+        switch inclusion.RequestMatch.MatchType {
+          // TODO: `winget list -s rewinged-local -q lapce` searches for the ProductCode with MatchType Exact
+          // Why does it use MatchType Exact?? Does the reference / official source normalize all ProductCodes on ingest??
+          case Exact:
+            if requestMatchValue == inclusion.RequestMatch.KeyWord {
+              // Break out of the inclusions loop after one successful match
+              anyInclusionMatched = true
+              break NEXT_INCLUSION
+            }
+          case CaseInsensitive:
+            if strings.EqualFold(requestMatchValue, inclusion.RequestMatch.KeyWord) {
+              // Break out of the inclusions loop after one successful match
+              anyInclusionMatched = true
+              break NEXT_INCLUSION
+            }
+          case StartsWith:
+            // StartsWith is implemented as case-sensitive, because it is that way in the reference implementation as well:
+            // https://github.com/microsoft/winget-cli-restsource/blob/01542050d79da0efbd11c0a5be543cb970b86eb9/src/WinGet.RestSource/Cosmos/PredicateGenerator.cs#L92-L102
+            if strings.HasPrefix(requestMatchValue, inclusion.RequestMatch.KeyWord) {
+              // Break out of the inclusions loop after one successful match
+              anyInclusionMatched = true
+              break NEXT_INCLUSION
+            }
+          case Substring:
+            // Substring comparison is case-insensitive, because it is that way in the reference implementation as well:
+            // https://github.com/microsoft/winget-cli-restsource/blob/01542050d79da0efbd11c0a5be543cb970b86eb9/src/WinGet.RestSource/Cosmos/PredicateGenerator.cs#L92-L102
+            if caseInsensitiveContains(requestMatchValue, inclusion.RequestMatch.KeyWord) {
+              // Break out of the inclusions loop after one successful match
+              anyInclusionMatched = true
+              break NEXT_INCLUSION
+            }
+          default:
+            // Unimplemented: Wildcard, Fuzzy, FuzzySubstring
+        }
+      }
+
+      // All filters and inclusions have passed for this manifest, add it to the returned map
+      if anyInclusionMatched {
+        fmt.Println("Adding to the results map:", packageIdentifier, "version", packageVersion.PackageVersion)
+        manifestResultsMap[packageIdentifier] = append(manifestResultsMap[packageIdentifier], packageVersion)
+      }
+    }
+  }
+  ms.RUnlock()
+
+  return manifestResultsMap
 }
 
 // Global variable that will hold all in-memory manifest data
