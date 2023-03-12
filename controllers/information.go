0eb59fb4d96edc3600452052b58000c5d4f8bfa5
jantari <jantari@github.com>
2023-03-12 20:06:13 +0100

    indicate support for 1.4.0 API to winget in /information endpoint

--- controllers/information.go
+++ controllers/information.go
@@ -9,7 +9,9 @@ import (
 func GetInformation(c *gin.Context) {
     response := new(models.API_Information_1_1_0)
     response.Data.SourceIdentifier = "rewinged"
-    response.Data.ServerSupportedVersions = []string{"1.1.0"}
+    // New API schema versions have to be included here or winget CLI client won't pick
+    // up the features / data fields from newer packages even if they are returned
+    response.Data.ServerSupportedVersions = []string{"1.1.0", "1.4.0"}
     c.JSON(200, response)
 }
 
